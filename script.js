214731127f32db6a734ea68c87009943f1395177
Egor Badmaev <60363270+htmlprogrammist@users.noreply.github.com>
2020-06-26 18:10:15 +0300

    feat: version 2.0, reconstruction todo

--- script.js
+++ script.js
@@ -1,43 +1,42 @@
-let List1 = $('#tdl1App ul')
-let Mask1 = 'tdl1_'
-
-let List2 = $('#tdl2App ul')
-let Mask2 = 'tdl2_'
-
-let List3 = $('#tdl3App ul')
-let Mask3 = 'tdl3_'
-
-let List4 = $('#tdl4App ul')
-let Mask4 = 'tdl4_'
+let big_object = {
+  lists: [
+    { list: $('#tdl1App ul') },
+    { list: $('#tdl2App ul') },
+    { list: $('#tdl3App ul') },
+    { list: $('#tdl4App ul') },
+  ],
+  masks: [
+    { mask: 'tdl1_' },
+    { mask: 'tdl2_' },
+    { mask: 'tdl3_' },
+    { mask: 'tdl4_' },
+  ],
+  element_id: [
+    { el: 0 },
+    { el: 0 },
+    { el: 0 },
+    { el: 0 },
+  ],
+  number_id: [
+    { num: 0 },
+    { num: 0 },
+    { num: 0 },
+    { num: 0 },
+  ]
+}
 
 function showTasks() {
   let Storage_size = localStorage.length
   if (Storage_size > 0) {
     for (let i = 0; i < Storage_size; i++) {
       let key = localStorage.key(i)
-      if (key.indexOf(Mask1) == 0) {
-        $('<li></li>').addClass('tdItem')
-          .attr('data-itemid', key)
-          .text(localStorage.getItem(key))
-          .appendTo(List1)
-      }
-      if (key.indexOf(Mask2) == 0) {
-        $('<li></li>').addClass('tdItem')
-          .attr('data-itemid', key)
-          .text(localStorage.getItem(key))
-          .appendTo(List2)
-      }
-      if (key.indexOf(Mask3) == 0) {
-        $('<li></li>').addClass('tdItem')
-          .attr('data-itemid', key)
-          .text(localStorage.getItem(key))
-          .appendTo(List3)
-      }
-      if (key.indexOf(Mask4) == 0) {
-        $('<li></li>').addClass('tdItem')
-          .attr('data-itemid', key)
-          .text(localStorage.getItem(key))
-          .appendTo(List4)
+      for (let j = 0; j <= 3; j++) {
+        if (key.indexOf(big_object.masks[j].mask) == 0) {
+          $('<li></li>').addClass('tdItem')
+            .attr('data-itemid', key)
+            .text(localStorage.getItem(key))
+            .appendTo(big_object.lists[j].list)
+        }
       }
     }
   }
@@ -45,89 +44,43 @@ function showTasks() {
 
 showTasks()
 
-$('#tdl1App input').on('keydown', function (event) {
-  if (event.keyCode != 13) return;
-  let str = event.target.value
-  event.target.value = ""
+
+function pressed(e, col) {
+
+  if (e.keyCode != 13) return;
+  let str = e.target.value;
+  e.target.value = ""
 
   if (str.length > 0) {
-    let number_Id_1 = 0
-    List1.children().each(function (index, el) {
-      let element_Id_1 = $(el).attr('data-itemid').slice(5)
-      if (element_Id_1 > number_Id_1) {
-        number_Id_1 = element_Id_1
-      }
+    big_object.number_id[col].num = 0
+    big_object.element_id[col].el = 0
+    big_object.lists[col].list.children().each(function (index, el) {
+      big_object.element_id[col].el = $(el).attr('data-itemid').slice(5);
+      if (big_object.element_id[col].el > big_object.number_id[col].num)
+        big_object.number_id[col].num = big_object.element_id[col].el;
     })
-    number_Id_1++;
-
-    localStorage.setItem(Mask1 + number_Id_1, str)
+    big_object.number_id[col].num++;
+    localStorage.setItem(big_object.masks[col].mask + big_object.number_id[col].num, str)
     $('<li></li>').addClass('tdItem')
-      .attr('data-itemid', Mask1 + number_Id_1)
-      .text(str).appendTo(List1)
+      .attr('data-itemid', big_object.masks[col].mask + big_object.number_id[col].num)
+      .text(str).appendTo(big_object.lists[col].list);
   }
+}
+
+$('#tdl1App input').on('keydown', function (e) {
+  pressed(e, 0)
 })
-
 $('#tdl2App input').on('keydown', function (e) {
-  if (e.keyCode != 13) return;
-  var str = e.target.value;
-  e.target.value = "";
-  if (str.length > 0) {
-    var number_Id_2 = 0;
-    List2.children().each(function (index, el) {
-      var element_Id_2 = $(el).attr('data-itemid').slice(5);
-      if (element_Id_2 > number_Id_2)
-        number_Id_2 = element_Id_2;
-    })
-
-    number_Id_2++;
-    localStorage.setItem(Mask2 + number_Id_2, str);
-    $('<li></li>').addClass('tdItem')
-      .attr('data-itemid', Mask2 + number_Id_2)
-      .text(str).appendTo(List2);
-  }
-
-});
+  pressed(e, 1)
+})
 $('#tdl3App input').on('keydown', function (e) {
-  if (e.keyCode != 13) return;
-  var str = e.target.value;
-  e.target.value = "";
-  if (str.length > 0) {
-    var number_Id_3 = 0;
-    List3.children().each(function (index, el) {
-      var element_Id_3 = $(el).attr('data-itemid').slice(5);
-      if (element_Id_3 > number_Id_3)
-        number_Id_3 = element_Id_3;
-    })
-
-    number_Id_3++;
-    localStorage.setItem(Mask3 + number_Id_3, str);
-    $('<li></li>').addClass('tdItem')
-      .attr('data-itemid', Mask3 + number_Id_3)
-      .text(str).appendTo(List3)
-  }
-});
-
+  pressed(e, 2)
+})
 $('#tdl4App input').on('keydown', function (e) {
-  if (e.keyCode != 13) return;
-  var str = e.target.value
-  e.target.value = "";
-  if (str.length > 0) {
-    var number_Id_4 = 0;
-    List4.children().each(function (index, el) {
-      var element_Id_4 = $(el).attr('data-itemid').slice(5);
-      if (element_Id_4 > number_Id_4)
-        number_Id_4 = element_Id_4;
-    })
-    number_Id_4++;
-    localStorage.setItem(Mask4 + number_Id_4, str);
-    $('<li></li>').addClass('tdItem')
-      .attr('data-itemid', Mask4 + number_Id_4)
-      .text(str).appendTo(List4);
-  }
-});
-
+  pressed(e, 3)
+})
 $(document).on('click', '.tdItem', function (e) {
-  var jet = $(e.target);
+  let jet = $(e.target)
   localStorage.removeItem(jet.attr('data-itemid'));
   jet.remove();
 })
